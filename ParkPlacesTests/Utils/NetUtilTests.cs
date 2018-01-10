2c54de83051ac7a8bf33785c9dcd82e554ed023f
Mihaly Meszaros <mihaly044@outlook.com>
2018-01-10 12:38:48 +0100

    Added NetUtil tests

---
+++ ParkPlacesTests/Utils/NetUtilTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkPlaces.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkPlaces.Utils.Tests
+{
+    [TestClass()]
+    public class NetUtilTests
+    {
+        [TestMethod()]
+        public void GetStringFromUrlTest()
+        {
+            string url = "https://pastebin.com/raw/apGeNVWT";
+            string expected = "test";
+
+            string actual = NetUtil.GetStringFromUrl(url);
+            Assert.AreEqual(expected, actual, actual);
+        }
+    }
+}
