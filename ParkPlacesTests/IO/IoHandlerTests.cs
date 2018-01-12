cd6e1bc186add17631f55a7f1dd87f519dfb9283
Mihaly Meszaros <mihaly044@outlook.com>
2018-01-12 09:52:46 +0100

    Added IoHandler tests

---
+++ ParkPlacesTests/IO/IoHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkPlaces.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkPlaces.IO.Tests
+{
+    [TestClass()]
+    public class IoHandlerTests
+    {
+        [TestMethod()]
+        public async void UpdateAsyncTest()
+        {
+            var result = await IoHandler.Instance.UpdateAsync(false, true);
+            Assert.IsNotNull(result);
+        }
+    }
+}
