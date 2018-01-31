5829699d2232acdde4fb65c117df5c45a18590bf
Mihaly <mihaly044@outlook.com>
2018-01-31 16:54:31 +0100

    Code cleanup

--- ParkPlaces/Extensions/Generic.cs
+++ ParkPlaces/Extensions/Generic.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ParkPlaces.Extensions
 {
     public static class Generic
     {
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
-        (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
         {
             HashSet<TKey> seenKeys = new HashSet<TKey>();
             foreach (TSource element in source)
