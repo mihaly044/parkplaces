1f0bd4bd2eadc354b7d05dedb1cde3ee4c7d2858
Mihaly <mihaly044@outlook.com>
2018-03-05 21:07:42 +0100

    Create database if not exists, GetConnection now accepts a noDatabase parameter

--- ParkPlaces/IO/Sql.cs
+++ ParkPlaces/IO/Sql.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using ParkPlaces.Extensions;
 using System.Collections.Specialized;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 using CryptSharp;
 using ParkPlaces.Misc;
 
@@ -24,6 +26,7 @@ namespace ParkPlaces.IO
         public Sql()
         {
             LoadDbCredientals();
+            SetupDb();
         }
 
         public Sql(string server="", string database="", string user="", string password="", string port="3306")
@@ -33,24 +36,212 @@ namespace ParkPlaces.IO
             User = user;
             Password = password;
             Port = port;
+            SetupDb();
+        }
+
+        private void SetupDb()
+        {
+            using (var cmd =
+                new MySqlCommand("SELECT COUNT(*) FROM information_schema.schemata WHERE SCHEMA_NAME = @dbName")
+                    {Connection = GetConnection(true)})
+            {
+                cmd.Parameters.AddWithValue("@dbName", Database);
+                var count = cmd.ExecuteScalar();
+
+                if (int.Parse(count.ToString()) >= 1) return;
+                // Database does not exist. Create it here
+                using (var cmd1 = new MySqlCommand(@"-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2018. Már 05. 20:22
+-- Kiszolgáló verziója: 10.1.30-MariaDB
+-- PHP verzió: 7.2.1
+
+SET SQL_MODE = ""NO_AUTO_VALUE_ON_ZERO"";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = ""+00:00"";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `parkplaces`
+--
+CREATE DATABASE IF NOT EXISTS `parkplaces` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `parkplaces`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `city` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `geometry`
+--
+
+CREATE TABLE `geometry` (
+  `zoneid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `groupid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `groupid`) VALUES
+(1, 'admin', '$2y$10$naypQWIa7gb7H8QLIUWa9.I8K3J3fh0SIp3AdOmnYpApBpnrC/KjG', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `zones`
+--
+
+CREATE TABLE `zones` (
+  `id` int(11) NOT NULL,
+  `cityid` int(11) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `fee` int(11) NOT NULL,
+  `service_na` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `timetable` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- A tábla indexei `geometry`
+--
+ALTER TABLE `geometry`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `zones`
+--
+ALTER TABLE `zones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cityid` (`cityid`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+
+--
+-- AUTO_INCREMENT a táblához `geometry`
+--
+ALTER TABLE `geometry`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25945;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT a táblához `zones`
+--
+ALTER TABLE `zones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1129;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `zones`
+--
+ALTER TABLE `zones`
+  ADD CONSTRAINT `zones_ibfk_1` FOREIGN KEY (`cityid`) REFERENCES `cities` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+")
+                    { Connection = GetConnection(true)})
+                {
+                    cmd1.ExecuteNonQuery();
+                }
+            }
         }
 
         private void LoadDbCredientals()
         {
             var dbSect = ConfigurationManager.GetSection("DBConnection") as NameValueCollection;
             if (dbSect == null) return;
-            Server = dbSect["server"].ToString();
-            Database = dbSect["database"].ToString();
-            User = dbSect["user"].ToString();
-            Password = dbSect["password"].ToString();
+            Server = dbSect["server"];
+            Database = dbSect["database"];
+            User = dbSect["user"];
+            Password = dbSect["password"];
             Port = dbSect["port"];
         }
 
 
-        public MySqlConnection GetConnection()
+        public MySqlConnection GetConnection(bool noDatabase = false)
         {
-            var mySqlConnection = new MySqlConnection(
-                $@"SERVER={Server};PORT={Port};DATABASE={Database};UID={User};PASSWORD={Password}");
+            MySqlConnection mySqlConnection;
+
+            if (noDatabase)
+            {
+                mySqlConnection = new MySqlConnection(
+                    $@"SERVER={Server};PORT={Port};UID={User};PASSWORD={Password}");
+            }
+            else
+            {
+                mySqlConnection = new MySqlConnection(
+                    $@"SERVER={Server};PORT={Port};DATABASE={Database};UID={User};PASSWORD={Password}");
+            }
 
             try
             {
@@ -58,8 +249,13 @@ namespace ParkPlaces.IO
             }
             catch (MySqlException e)
             {
-                Debug.Write("Mysql error: " + e.Message);
-                return null;
+#if DEBUG
+                Debug.WriteLine("Mysql error: " + e.Message);
+                Debugger.Break();
+#else
+                MessageBox.Show("Fatal database error. The application is going to close.", "Error" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+#endif
             }
             return mySqlConnection;
         }
@@ -72,14 +268,11 @@ namespace ParkPlaces.IO
             using (var cmd = new MySqlCommand("SELECT * FROM users WHERE username = @username ")
             { Connection = GetConnection() })
             {
-                cmd.Parameters.Add("@username", MySqlDbType.String);
-                cmd.Parameters[0].Value = username;
+                cmd.Parameters.AddWithValue("@username", username);
 
                 using(var reader = cmd.ExecuteReader())
                 {
-                    reader.Read();
-
-                    if (reader.HasRows)
+                    if(reader.Read() && reader.HasRows)
                         if (Crypter.CheckPassword(password, reader["password"].ToString()))
                             return (GroupRole)Enum.Parse(typeof(GroupRole), reader["groupid"].ToString());
                 }
