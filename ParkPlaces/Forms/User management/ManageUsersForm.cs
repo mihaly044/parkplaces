372df415e0beb0b9d119aee8aec31a8046f1aa7d
Mihaly Meszaros <mihaly044@outlook.com>
2018-03-08 13:09:56 +0100

    Removed unused usings

--- ParkPlaces/Forms/User management/ManageUsersForm.cs
+++ ParkPlaces/Forms/User management/ManageUsersForm.cs
@@ -3,12 +3,6 @@ using ParkPlaces.IO;
 using ParkPlaces.Misc;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ParkPlaces.Forms
