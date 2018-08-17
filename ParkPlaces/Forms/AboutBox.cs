0d06326810f01c54751e66a7b0e40bae93b9873c
Mihaly Meszaros <mihaly044@outlook.com>
2018-08-17 22:24:21 +0200

    Code cleanup

--- ParkPlaces/Forms/AboutBox.cs
+++ ParkPlaces/Forms/AboutBox.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Windows.Forms;
 // ReSharper disable VirtualMemberCallInConstructor
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace ParkPlaces.Forms
 {
