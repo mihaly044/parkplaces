2a1d5ceb7f68342a18f9646a5a2f1b8a602bccc9
mihaly044 <mihaly044@gmail.com>
2018-09-16 16:19:31 +0200

    Fixed SQL dump

--- parkplaces.sql
+++ parkplaces.sql
@@ -1,152 +1,78 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET FOREIGN_KEY_CHECKS=0;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `parkplaces`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cities`
---
-
+-- ----------------------------
+-- Table structure for cities
+-- ----------------------------
+DROP TABLE IF EXISTS `cities`;
 CREATE TABLE `cities` (
-  `id` int(11) NOT NULL,
-  `city` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of cities
+-- ----------------------------
+INSERT INTO `cities` VALUES ('1', 'Szeged');
+INSERT INTO `cities` VALUES ('2', 'Budapest');
 
---
--- Table structure for table `geometry`
---
-
+-- ----------------------------
+-- Table structure for geometry
+-- ----------------------------
+DROP TABLE IF EXISTS `geometry`;
 CREATE TABLE `geometry` (
   `zoneid` int(11) NOT NULL,
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `lat` double NOT NULL,
   `lng` double NOT NULL,
-  `polyindex` int(255) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `polyindex` int(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `geometry`
---
+-- ----------------------------
+-- Records of geometry
+-- ----------------------------
+INSERT INTO `geometry` VALUES ('1', '1', '47.49540043878657', '19.037632942199707', '0');
+INSERT INTO `geometry` VALUES ('1', '2', '47.50201104821246', '19.041152000427246', '1');
+INSERT INTO `geometry` VALUES ('1', '3', '47.49783692937882', '19.041036665439606', '4');
+INSERT INTO `geometry` VALUES ('1', '4', '47.49653125993388', '19.042611122131348', '2');
+INSERT INTO `geometry` VALUES ('1', '5', '47.497995492159504', '19.04108762741089', '3');
 
-INSERT INTO `geometry` (`zoneid`, `id`, `lat`, `lng`, `polyindex`) VALUES
-(1615, 32748, 47.500313173300995, 19.05316561460495, 0),
-(1615, 32749, 47.494702724112315, 19.05340164899826, 1),
-(1615, 32750, 47.49463023376596, 19.062521159648895, 2),
-(1615, 32751, 47.500008745704335, 19.062349498271942, 3),
-(1615, 32752, 47.49704592046888, 19.055120944976807, 5),
-(1615, 32753, 47.4971039100721, 19.0598201751709, 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
   `groupid` int(11) NOT NULL,
-  `creatorid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `creatorid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `users`
---
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('1', 'admin', '$2a$06$4K7V4/gHrrpl0oL/E.xCnOepsfTle9VdxBmUEZbijFZOpehMr9WAq', '4', '0');
 
-INSERT INTO `users` (`id`, `username`, `password`, `groupid`, `creatorid`) VALUES
-(1, 'admin', '$2a$06$4K7V4/gHrrpl0oL/E.xCnOepsfTle9VdxBmUEZbijFZOpehMr9WAq', 4, 0)
-
--- --------------------------------------------------------
-
---
--- Table structure for table `zones`
---
-
+-- ----------------------------
+-- Table structure for zones
+-- ----------------------------
+DROP TABLE IF EXISTS `zones`;
 CREATE TABLE `zones` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `cityid` int(11) NOT NULL,
   `color` varchar(255) NOT NULL,
   `fee` int(11) NOT NULL,
   `service_na` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `timetable` varchar(255) DEFAULT NULL,
-  `common_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `common_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cities`
---
-ALTER TABLE `cities`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Indexes for table `geometry`
---
-ALTER TABLE `geometry`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `zones`
---
-ALTER TABLE `zones`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cityid` (`cityid`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cities`
---
-ALTER TABLE `cities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2256;
-
---
--- AUTO_INCREMENT for table `geometry`
---
-ALTER TABLE `geometry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32784;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `zones`
---
-ALTER TABLE `zones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1626;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of zones
+-- ----------------------------
+INSERT INTO `zones` VALUES ('1', '2', '#FF8000', '0', 'Test', 'New zone', '1', '1');
