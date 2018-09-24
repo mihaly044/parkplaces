ac716b30013db0cb72319e42f1fec5486198ac22
mihaly044 <mihaly044@gmail.com>
2018-09-24 20:28:09 +0200

    16 KB buffer size

--- PPNetLib/Tcp/WatsonTcpClient.cs
+++ PPNetLib/Tcp/WatsonTcpClient.cs
@@ -423,7 +423,7 @@ namespace watsontcp_dotnetcore.Tcp
 
                     int read = 0;
                     byte[] buffer;
-                    long bufferSize = 4096;
+                    long bufferSize = 8192;
                     if (bufferSize > bytesRemaining)
                     {
                         bufferSize = bytesRemaining;
@@ -651,7 +651,7 @@ namespace watsontcp_dotnetcore.Tcp
 
                     int read = 0;
                     byte[] buffer;
-                    long bufferSize = 4096;
+                    long bufferSize = 8192;
                     if (bufferSize > bytesRemaining)
                     {
                         bufferSize = bytesRemaining;
