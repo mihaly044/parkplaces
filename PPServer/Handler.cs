aa33ff19bca667cfc0db30dd00404de25881634d
mihaly044 <mihaly044@gmail.com>
2018-09-25 21:24:36 +0200

    Revert "Don't use debug message"
    This reverts commit 838784fdc74f995cc869030eb886629515579392.

--- PPServer/Handler.cs
+++ PPServer/Handler.cs
@@ -45,7 +45,7 @@ namespace PPServer
             foreach(var zone in _server.Dto.Zones)
             {
                 var zoneSerialized = JsonConvert.SerializeObject(zone, Converter.Settings);
-                //ConsoleKit.Message(ConsoleKit.MessageType.DEBUG, "Now sending zone id {0}\n", zone.Id);
+                ConsoleKit.Message(ConsoleKit.MessageType.DEBUG, "Now sending zone id {0}\n", zone.Id);
                 if(!_server.Send(ipPort, new ZoneListAck() { Zone = zoneSerialized }))
                 {
                     ConsoleKit.Message(ConsoleKit.MessageType.ERROR, "Connection lost ...\n", zone.Id);
