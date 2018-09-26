649d21ff2ff624f21cb6f8c49a75b5b2a199de37
mihaly044 <mihaly044@gmail.com>
2018-09-26 21:12:26 +0200

    Removed unnecessary code

--- PPNetLib/Extensions/Generic.cs
+++ PPNetLib/Extensions/Generic.cs
@@ -11,15 +11,5 @@ namespace PPNetLib.Extensions
             long longSum = data.Sum(x => (long)x);
             return unchecked((byte)longSum);
         }
-
-        /// <summary>
-        /// Returns all the values from an enum
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static IEnumerable<T> GetValues<T>()
-        {
-            return Enum.GetValues(typeof(T)).Cast<T>();
-        }
     }
 }
