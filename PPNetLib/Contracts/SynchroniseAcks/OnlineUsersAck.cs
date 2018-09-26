8c798f6ae7a101d4bf64a1d6f849febd5949fe83
mihaly044 <mihaly044@gmail.com>
2018-09-27 00:14:20 +0200

    Fixed sending online users list

--- PPNetLib/Contracts/SynchroniseAcks/OnlineUsersAck.cs
+++ PPNetLib/Contracts/SynchroniseAcks/OnlineUsersAck.cs
@@ -7,6 +7,7 @@ namespace PPNetLib.Contracts.SynchroniseAcks
     [ProtoContract]
     public class OnlineUsersAck: Packet
     {
+        [ProtoMember(1)]
         public List<User> OnlineUsersList;
 
         public OnlineUsersAck()
