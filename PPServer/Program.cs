e65d9eea22f4b4e6b9a656ca604bfe4e7f1cad80
mihaly044 <mihaly044@gmail.com>
2018-09-28 23:16:28 +0200

    Sleep for debugging ...

--- PPServer/Program.cs
+++ PPServer/Program.cs
@@ -12,6 +12,8 @@ namespace PPServer
             Console.SetOut(writer);
             var server = new Server(writer);
 
+            System.Threading.Thread.Sleep(10);
+
             Parser.Default.ParseArguments<ServerOptions>(args)
                 .WithParsed<ServerOptions>(o=> {
                     if(o.LimitZones > 0)
